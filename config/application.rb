b7d81e37a522e7f6a504317d9d639f00dbb2a858
Rich Hoyt <rhoyt10@gmail.com>
2022-03-11 19:13:46 -0500

    Don't initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+config.assets.initialize_on_precompile = false
 
 Rails::Html::WhiteListSanitizer.allowed_tags << "iframe"
 # Require the gems listed in Gemfile, including any gems
@@ -12,6 +13,7 @@ module PersonalWebsite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     Rails::Html::WhiteListSanitizer.allowed_tags << "iframe"
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
